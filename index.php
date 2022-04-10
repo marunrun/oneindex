c06269fda4c18bf7f2972bcfab4994903b9c8826
marin <marunrun@163.com>
2022-04-10 18:56:47 +0800

    feat : 使用国际版 更新url

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 require  __DIR__.'/init.php';
 
 //世纪互联
-onedrive::$api_url = "https://microsoftgraph.chinacloudapi.cn/v1.0";
-onedrive::$oauth_url = "https://login.partner.microsoftonline.cn/common/oauth2/v2.0";
-onedrive::$onedrive_url = "https://portal.partner.microsoftonline.cn/";
+// onedrive::$api_url = "https://microsoftgraph.chinacloudapi.cn/v1.0";
+// onedrive::$oauth_url = "https://login.partner.microsoftonline.cn/common/oauth2/v2.0";
+// onedrive::$onedrive_url = "	/";
 
 /**
  *    程序安装
